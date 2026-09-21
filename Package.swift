3072de715e78fd7c586864a2f97ee3f521a6c84b
YoushengZhange <yzhang@fintechautomation.com>
2026-09-21 22:51:17 +0800

    update

--- Package.swift
+++ Package.swift
@@ -15,7 +15,26 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IdentityVerificationiOSSDK",
-            path: "IdentityVerificationiOSSDK.xcframework"
+            path: "./IdentityVerificationiOSSDK.xcframework"
         )
     ]
 )
+// xcodebuild archive \
+// -project _Pods.xcodeproj \
+// -scheme IdentityVerificationiOSSDK \
+// -destination "generic/platform=iOS" \
+// -archivePath "build/IdentityVerificationiOSSDK-iOS.xcarchive" \
+// SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+
+// xcodebuild archive \
+// -project _Pods.xcodeproj \
+// -scheme IdentityVerificationiOSSDK \
+// -destination "generic/platform=iOS Simulator" \
+// -archivePath "build/IdentityVerificationiOSSDK-iOS-Simulator.xcarchive" \
+// SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
+
+// xcodebuild -create-xcframework \
+// -framework build/IdentityVerificationiOSSDK-iOS.xcarchive/Products/Library/Frameworks/IdentityVerificationiOSSDK.framework \
+// -framework build/IdentityVerificationiOSSDK-iOS-Simulator.xcarchive/Products/Library/Frameworks/IdentityVerificationiOSSDK.framework \
+// -output build/IdentityVerificationiOSSDK.xcframework
